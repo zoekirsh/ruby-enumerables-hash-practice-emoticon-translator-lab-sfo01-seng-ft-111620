515d84d9916ac7b27821a83cc8e9b6e46eea891b
Zoe Kirsh <zoekirsh@gmail.com>
2020-10-19 01:45:28 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -1,13 +1,23 @@
-# require modules here
+require 'pry'
+require "yaml"
 
-def load_library
-  # code goes here
+
+def load_library(path)
+  emoji_library = {}
+  YAML.load_file(path).each do |key, value|
+    emoji_library[key] = {}
+    emoji_library[key][:english] = value[0]
+    emoji_library[key][:japanese] = value[1]
+  end
+  emoji_library
 end
 
-def get_japanese_emoticon
-  # code goes here
+
+def get_japanese_emoticon(hash, string)
+  # look up inner value and return other inner value 
+  
 end
 
-def get_english_meaning
-  # code goes here
+def get_english_meaning(hash, string)
+  # look up the inner value and return the outer key 
 end
